5f326418b91ab85b96a11aed575b4162a5abffc6
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-09-10 21:02:56 +0200

    ready to go

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,78 @@
-import Image from 'next/image'
+'use client'
+import { useState, ChangeEvent } from "react";
 
-export default function Home() {
+const Calculator: React.FC = () => {
+  const [yearlyIncome, setYearlyIncome] = useState<string | number>(""); // Can be a string or a number to allow for empty input
+  const [givebackRatePercent, setGivebackRatePercent] = useState<string | number>(""); // Can be a string or a number to allow for empty input
+  const [result, setResult] = useState<Record<string, number> | null>(null); // Result is either null or an object with number values
+
+  const calculate = () => {
+    if (typeof yearlyIncome === "string" || typeof givebackRatePercent === "string") return;
+
+    const hourlyRate = yearlyIncome / 84600;
+    const givebackRate = (givebackRatePercent / 100) * hourlyRate;
+    const tonnePrice = 14;
+    const SCC = 26000;
+    const ROI = SCC / tonnePrice;
+    const giveBackValue = givebackRate * ROI;
+
+    const hourlyNet = hourlyRate + giveBackValue;
+    const minutlyNet = hourlyNet / 60;
+    const secondlyNet = minutlyNet / 60;
+    const microsecondNet = secondlyNet / 1000;
+
+    const tonnePerHour = SCC / hourlyNet;
+    const kgPerMinute = SCC / minutlyNet / 1000;
+    const gramPerSecond = SCC / secondlyNet / 1000;
+
+    setResult({
+      hourlyNet,
+      minutlyNet,
+      secondlyNet,
+      microsecondNet,
+      tonnePerHour,
+      kgPerMinute,
+      gramPerSecond,
+    });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="container mx-auto p-8 bg-gray-800 ">
+      <h1 className="">Tarif</h1>
+      <label>
+        Yearly Income ($):&nbsp;
+        <input
+          type="number"
+          value={yearlyIncome}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setYearlyIncome(Number(e.target.value))}
         />
-      </div>
+      </label>
+      <br />
+      <label>
+        Giveback Rate (%):&nbsp;
+        <input
+          type="number"
+          value={givebackRatePercent}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setGivebackRatePercent(Number(e.target.value))}
+        />
+      </label>
+      <br />
+      <button onClick={calculate}>Calculate</button>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {result && (
+        <div>
+          <h2>Results:</h2>
+          <div>Hourly Net: €{result.hourlyNet.toFixed(2)}</div>
+          <div>Minutly Net: €{result.minutlyNet.toFixed(2)}</div>
+          <div>Secondly Net: €{result.secondlyNet.toFixed(2)}</div>
+          <div>Microsecond Net: €{result.microsecondNet.toFixed(6)}</div>
+          <div>Tonnes per Hour: {result.tonnePerHour.toFixed(2)}</div>
+          <div>kg per Minute: {result.kgPerMinute.toFixed(2)}</div>
+          <div>g per Second: {result.gramPerSecond.toFixed(2)}</div>
+        </div>
+      )}
+    </div>
+  );
+};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Calculator;
