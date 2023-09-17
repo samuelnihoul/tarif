ac888f3fcd22a78338377b502db0f6857480849f
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-09-17 17:31:35 +0200

    beautiful buttons

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,8 @@ const Calculator: React.FC = () => {
   const [givebackRatePercent, setGivebackRatePercent] = useState<string | number>(""); // Can be a string or a number to allow for empty input
   const [result, setResult] = useState<Record<string, number> | null>(null); // Result is either null or an object with number values
 
-  const calculate = () => {
+  const calculate = (x: number = 1) => {
+
     if (typeof yearlyIncome === "string" || typeof givebackRatePercent === "string") return;
 
     const hourlyRate = yearlyIncome / 84600;
@@ -17,24 +18,24 @@ const Calculator: React.FC = () => {
     const giveBackValue = givebackRate * ROI; // debatable, hp of instantaneity, hp of no double people-crediting
     const carbonIntensity = 0.26 // kg/USD, https://www.iea.org/data-and-statistics/charts/co2-emissions-intensity-of-gdp-1990-2021  , world
     const salaryCost = hourlyRate * carbonIntensity * SCC / 1000
-    const hourlyNet = hourlyRate + giveBackValue - salaryCost;
+    const hourlyNet = (hourlyRate + giveBackValue - salaryCost) * x
     const minutlyNet = hourlyNet / 60;
     const secondlyNet = minutlyNet / 60;
     const microsecondNet = secondlyNet / 1000;
-    const tonnePerHour = SCC / hourlyNet;
-    const kgPerMinute = SCC / minutlyNet / 1000;
-    const gramPerSecond = SCC / secondlyNet / 1000;
+    const hourPerTonne = hourlyNet / SCC;
+    const minutePerKg = minutlyNet / (SCC / 1000);
+    const secondPerGram = secondlyNet / (SCC / 1000000);
 
     setResult({
       hourlyNet,
       minutlyNet,
       secondlyNet,
       microsecondNet,
-      tonnePerHour,
-      kgPerMinute,
-      gramPerSecond,
-    });
-  };
+      hourPerTonne,
+      minutePerKg,
+      secondPerGram,
+    })
+  }
 
   return (
     <div className="container mx-auto p-8 bg-gray-800 ">
@@ -59,7 +60,7 @@ const Calculator: React.FC = () => {
         />
       </label>
       <br />
-      <button onClick={calculate} className="text-m p-10">Calculate</button>
+      <button onClick={() => calculate()} className="text-m p-2 m-2 bg-green-500 text-white rounded">Calculate</button>
 
       {result && (
         <div>
@@ -71,9 +72,12 @@ const Calculator: React.FC = () => {
           <div>Microsecond Net: ${result.microsecondNet.toFixed(6)}</div>
           <br />
           Values in the second section say how much CO2 you are saving per unit of time. Any action more efficient than that is a must do, anything below is a pass.
-          <div>Tonnes per Hour: {result.tonnePerHour.toFixed(2)}</div>
-          <div>kg per Minute: {result.kgPerMinute.toFixed(2)}</div>
-          <div>g per Second: {result.gramPerSecond.toFixed(2)}</div><br />
+          <div>hour per tonne: {result.hourPerTonne.toFixed(2)}</div>
+          <div>minute per kg: {result.minutePerKg.toFixed(2)}</div>
+          <div>second per gram: {result.secondPerGram.toFixed(2)}</div><br />
+          <button onClick={() => calculate(10)} className="m-2 p-2 bg-yellow-500 text-white rounded">10x (focus mode)</button>
+          <button onClick={() => calculate(100)} className="m-2 p-2 bg-red-500 text-white rounded">100x (sprint mode)
+          </button>
         </div>
       )}
     </div>
